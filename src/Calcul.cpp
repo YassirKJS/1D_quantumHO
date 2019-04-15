b201c306fc65d1363ee1353ca38e87aa636a25fc
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-04-15 11:50:01 +0200

    updated the hermite polynomial function

--- src/Calcul.cpp
+++ src/Calcul.cpp
@@ -33,22 +33,39 @@ mat Calcul::calculPolynomeHermite()
 {
     mat H(n_max, z.n_elem);
 
-    for (int i = 0; i < z.n_elem; ++i)
+    if(n_max == 0)
     {
-        H(0, i) = 1;
-    }
-    rowvec h2 = rowvec(z.n_elem);
-    h2 = 2 * z;
-
-    H.row(1) = h2;
-
-    for (int i = 2; i < n_max; i++)
-    {
-        rowvec hn = rowvec(z.n_elem);
-        hn = h2 % H.row(i-1) - (2*i) * H.row(i-2);
-        H.row(i) = hn;
+        H = z.ones(size(z));
     }
 
+    else
+    {
+        if(n_max == 1)
+        {
+            return z.for_each([](arma::mat::elem_type & val)
+            {
+                val = 2 * val;
+            });
+        }
+        else
+        {
+            for(int i = 0; i < z.n_elem; ++i)
+            {
+                H(0, i) = 1;
+            }
+            rowvec h2 = rowvec(z.n_elem);
+            h2 = 2 * z;
+
+            H.row(1) = h2;
+
+            for(int i = 2; i < n_max; i++)
+            {
+                rowvec hn = rowvec(z.n_elem);
+                hn = h2 % H.row(i - 1) - (2 * i) * H.row(i - 2);
+                H.row(i) = hn;
+            }
+        }
+    }
     return H;
 }
 
