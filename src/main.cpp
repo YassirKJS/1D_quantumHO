f9646ddf4931a1c745df2d8af07ce2f125362e59
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-04-23 22:49:48 +0200

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,26 @@
+#include <iostream>
 #include <armadillo>
-int main()
+#include "Calcul.h"
+#include "DataInterface.h"
+#include "Miscellaneous.h"
+using namespace arma;
+using namespace std;
+
+void test()
 {
-  
+  //mat z = rowvec({-2, -1, 0, 1, 2});
+  Calcul *c = new Calcul();
+  DataInterface d;
+  int n = 5;
+  rowvec z = d.read(n);
+
+  z.print();
+  cout << n << endl;
+
+  c->setN(n);
+  c->setZ(z);
+
+  d.write(z, c->calculWn());
+  // d.write(z, c->calculPolynomeHermite());
 }
+
