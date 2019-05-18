07bf7caee84a18e9f833b76822f5a38b7c7e2f1b
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-05-18 23:09:21 +0200

    re: deriv function comment

--- src/Miscellaneous.cpp
+++ src/Miscellaneous.cpp
@@ -27,6 +27,18 @@ int Miscellaneous::factorial(int n)
 		return n * factorial(n - 1); // recursive call to factorial()
 }
 
+/**
+ * Computing the derivative of a function
+ *
+ * this function approximates the fist order derivative
+ *
+ * @param Y1 vector containing the n values of the image
+ * @param Y2 vector containing the n values of the image offset by one value
+ * @param X1 vector containing the n points where the image is valued
+ * @param X2 vector containing the n points where the image is valued offset by one value
+ *
+ * @return derv vector containing the values of the derived function
+ */
 arma::mat Miscellaneous::deriv(arma::mat Y1, arma::mat Y2, arma::mat X1, arma::mat X2)
 {
   arma::mat derv = (Y2 - Y1) / (X2 - X1);
